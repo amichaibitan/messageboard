31d70dd8c9941f0493112d36f8b75a3679fa4ff8
amichaibitan <74491411+amichaibitan@users.noreply.github.com>
2021-03-23 21:52:13 +0200

    Delete messageboard-a-main directory

--- messageboard-a-main/Board.cpp
+++
@@ -1,9 +0,0 @@
-#include "Board.hpp"
-
-
-namespace ariel {
-     void Board::post(unsigned int row,unsigned int col, Direction dir, string str){}
-     string Board::read(unsigned int row,unsigned int col, Direction dir,int len){
-         return "1";
-     }
-     void Board::show(){}}
